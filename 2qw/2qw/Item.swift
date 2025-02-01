a065c3520399247485dc0741a14c2808a472d860
Illidian4368 <illidian4368@gmail.com>
2025-02-01 17:45:32 +0100

    Create Package.swift

---
+++ 2qw/2qw/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  2qw
+//
+//  Created by PF on 01/02/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ 2qw/2qw/_qwApp.swift
@@ -0,0 +1,32 @@
+//
+//  _qwApp.swift
+//  2qw
+//
+//  Created by PF on 01/02/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct _qwApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
