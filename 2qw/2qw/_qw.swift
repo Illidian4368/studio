ee6173b6f07e00e058a6b1f01fc756424ab05f28
Illidian4368 <illidian4368@gmail.com>
2025-02-01 18:32:20 +0100

    Create _qw.swift
    Co-Authored-By: Morten Daniel Fornes <hi@brun.sh>

---
+++ 2qw/2qw/_qw.swift
@@ -0,0 +1,18 @@
+//
+//  _qw.swift
+//  2qw
+//
+//  Created by PF on 01/02/2025.
+//
+
+import Foundation
+
+/// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
+class _qw: NSObject, _qwProtocol {
+    
+    /// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+    @objc func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void) {
+        let response = firstNumber + secondNumber
+        reply(response)
+    }
+}
